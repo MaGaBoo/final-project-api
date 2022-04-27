140a0b504b9d6369c042e39daa2f53e72c76814c
Robert Cabrera <cabrera.rch@gmail.com>
2022-04-27 15:37:44 +0200

    feat: edit plant changing image

--- config/routes.js
+++ config/routes.js
@@ -1,38 +1,41 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const upload = require('./storage.config')
+const upload = require("./storage.config");
 
-const authMiddleware = require('../middlewares/auth.middleware')
+const authMiddleware = require("../middlewares/auth.middleware");
 
-const plantController = require('../controllers/plant.controller');
-const usersController = require('../controllers/users.controller');
-const authController = require('../controllers/auth.controller');
+const plantController = require("../controllers/plant.controller");
+const usersController = require("../controllers/users.controller");
+const authController = require("../controllers/auth.controller");
 
-router.get('/', (req, res, next) => {
-    console.log('holi, soy la home');
-    res.status(200).json({ ok: true });
+router.get("/", (req, res, next) => {
+  console.log("holi, soy la home");
+  res.status(200).json({ ok: true });
 });
 
-
 /* Plant */
-router.post('/plant/new', upload.single('image'), plantController.create);
-router.get('/plant/:id', plantController.detail);
-router.get('/plant', plantController.list);
-router.patch('/plant/:id', plantController.update);
-router.delete('/plant/:id', plantController.delete);
-
+router.post("/plant/new", upload.single("image"), plantController.create);
+router.get("/plant/:id", plantController.detail);
+router.get("/plant", plantController.list);
+router.patch("/plant/:id", upload.single("image"), plantController.update);
+router.delete("/plant/:id", plantController.delete);
 
 /* Auth */
-router.post('/login', authMiddleware.isNotAuthenticated, authController.login)
-
+router.post("/login", authMiddleware.isNotAuthenticated, authController.login);
 
 /* Users */
-router.post('/users', authController.create);
-router.get('/users/me', authMiddleware.isAuthenticated, usersController.getCurrentUser);
-router.get('/users/:id', usersController.getUserById);
-router.post('/shoppingCart', authMiddleware.isAuthenticated, usersController.checkout);
-
-
+router.post("/users", authController.create);
+router.get(
+  "/users/me",
+  authMiddleware.isAuthenticated,
+  usersController.getCurrentUser
+);
+router.get("/users/:id", usersController.getUserById);
+router.post(
+  "/shoppingCart",
+  authMiddleware.isAuthenticated,
+  usersController.checkout
+);
 
 module.exports = router;
