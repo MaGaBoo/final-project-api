671c8614f940ae4c53a157f0393aa343bc5a75cf
Robert Cabrera <cabrera.rch@gmail.com>
2022-04-27 10:23:29 +0200

    feat: creat plant with image

--- controllers/plant.controller.js
+++ controllers/plant.controller.js
@@ -1,42 +1,40 @@
-const Plant = require('../models/Plant.Model.js');
+const Plant = require("../models/Plant.Model.js");
 
 module.exports.create = (req, res, next) => {
-  const plant = { user, content } = req.body
-/*   console.log(req.body) */
+  let plant = ({ user, content, plantCare } = req.body);
 
-/*   console.log('new plant:', JSON.parse(plant)) */
+  plant.plantCare = JSON.parse(plantCare);
 
   if (req.file) {
-    plant.image = req.file.path
+    plant.image = req.file.path;
   }
 
-    Plant.create(plant)
-      .then(plant => res.status(200).json(plant))
-      .catch(next)
-}
+  Plant.create(plant)
+    .then((plant) => res.status(200).json(plant))
+    .catch(next);
+};
 
 module.exports.detail = (req, res, next) => {
   Plant.findById(req.params.id)
-    .populate('user')
-    .then(plant => res.status(200).json(plant))
-    .catch(next)
-}
+    .populate("user")
+    .then((plant) => res.status(200).json(plant))
+    .catch(next);
+};
 
 module.exports.list = (req, res, next) => {
   Plant.find()
-    .then(plants => res.status(200).json(plants))
-    .catch(next)
-}
-
+    .then((plants) => res.status(200).json(plants))
+    .catch(next);
+};
 
 module.exports.update = (req, res, next) => {
   Plant.findByIdAndUpdate(req.params.id, req.body, { new: true })
-    .then(plant => res.status(200).json(plant))
-    .catch(next)
-}
+    .then((plant) => res.status(200).json(plant))
+    .catch(next);
+};
 
 module.exports.delete = (req, res, next) => {
   Plant.findByIdAndRemove(req.params.id)
-    .then(plant => res.status(200).json(plant))
-    .catch(next)
-}
+    .then((plant) => res.status(200).json(plant))
+    .catch(next);
+};
