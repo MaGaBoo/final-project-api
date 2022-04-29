98cd2657dc99551bbaed228f374b0912cf8c9af1
Robert Cabrera <cabrera.rch@gmail.com>
2022-04-29 09:25:03 +0200

    feat: change order view and created order detail

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -2,14 +2,14 @@ const User = require('../models/User.model');
 const Order = require('../models/Order.Model');
 
 module.exports.detail = (req, res, next) => {
-
-    Order.findById(req.params.id)
-    .then((order) => {
-        res.status(200).json(order)
-        console.log(order)
-    
-    })
-    .catch(error => console.log(error))
+  Order.findById(req.params.id)
+    .then((order) => res.status(200).json(order))
+    .catch(next);
 }
 
+module.exports.list = (req, res, next) => {
+  Order.find()
+    .then((orders) => res.status(200).json(orders))
+    .catch(next);
+}
 
